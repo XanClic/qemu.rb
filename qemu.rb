35e4c070cf29ce9290b4c192eb289c553a256349
Max Reitz <xanclic@xanclic.moe>
2018-05-01 19:32:48 +0200

    qemu.rb: Use qtest mode by default

--- qemu.rb
+++ qemu.rb
@@ -28,7 +28,16 @@ class VM
         @this_vm = $vm_counter
         $vm_counter += 1
 
-        @qmp_socket = UNIXServer.new('/tmp/qemu.rb-qmp-' + @this_vm.to_s)
+        args = command_line.select { |cla| cla.kind_of?(Symbol) }
+        command_line.reject! { |cla| cla.kind_of?(Symbol) }
+
+        qtest = !args.include?(:noqtest)
+
+        @qmp_socket_fname = '/tmp/qemu.rb-qmp-' + @this_vm.to_s
+        @qtest_socket_fname = '/tmp/qemu.rb-qtest-' + @this_vm.to_s if qtest
+
+        @qmp_socket = UNIXServer.new(@qmp_socket_fname)
+        @qtest_socket = UNIXServer.new(@qtest_socket_fname) if qtest
 
         @stdout, c_stdout = IO.pipe()
         @stderr, c_stderr = IO.pipe()
@@ -37,8 +46,14 @@ class VM
         if !@child
             STDOUT.reopen(c_stdout)
             STDERR.reopen(c_stderr)
-            Process.exec(*command_line, '-qmp', 'unix:/tmp/qemu.rb-qmp-' + @this_vm.to_s,
-                                        '-M', 'q35,accel=qtest:tcg', '-display', 'none')
+            if qtest
+                Process.exec(*command_line, '-qmp', 'unix:' + @qmp_socket_fname,
+                                            '-M', 'q35,accel=qtest:tcg', '-display', 'none',
+                                            '-qtest', 'unix:' + @qtest_socket_fname)
+            else
+                Process.exec(*command_line, '-qmp', 'unix:' + @qmp_socket_fname,
+                                            '-M', 'q35,accel=tcg', '-display', 'none')
+            end
             exit 1
         end
 
@@ -52,6 +67,18 @@ class VM
         end
 
         @qmp
+    end
+
+    def qtest
+        if !@qtest_socket
+            return nil
+        end
+
+        if !@qtest_con
+            @qtest_con = @qtest_socket.accept()
+        end
+
+        @qtest_con
     end
 
     def stdout
@@ -68,7 +95,8 @@ class VM
 
         @child = nil
         begin
-            File.delete('/tmp/qemu.rb-qmp-' + @this_vm.to_s)
+            File.delete(@qmp_socket_fname)
+            File.delete(@qtest_socket_fname) if @qtest_socket_fname
         rescue
         end
     end
